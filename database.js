6f1bef5d8dcc0ea7e9222531c0b42a1e75203886
Ows.borges <ows.borges@loginlogistica.com.br>
2026-04-06 19:46:01 -0300

    teste

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 
+console.log("=== DIAGNÓSTICO DE BANCO DE DADOS ===");
+console.log("A variável DATABASE_URL foi encontrada pelo Node? ->", !!process.env.DATABASE_URL);
+
 const prisma = new PrismaClient();
+
 module.exports = prisma;
