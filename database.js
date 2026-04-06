b2212df587b2c8214c2008f9e37bc303b52a03af
Ows.borges <ows.borges@loginlogistica.com.br>
2026-04-06 15:51:53 -0300

    AJUSTE

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+process.env.DATABASE_URL =
+    "postgresql://postgres.djmambmnusfonxvvhape:Botguilda123@aws-0-sa-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true&connection_limit=1";
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
